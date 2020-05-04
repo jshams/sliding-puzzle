a55a5033ef95494a8de5c7f96cf1c86700269427
Jake Shams <jshams62@gmail.com>
2020-05-03 20:49:48 -0700

    added improved solver that can solve all size board (solving speed is not near optimal)

--- frontend/game-components/solver.js
+++ frontend/game-components/solver.js
@@ -18,7 +18,6 @@ class Solver {
         firstBoard.moves = []
         this.queue.push(firstBoard)
         this.seen[firstBoard.state] = true
-        return false
     }
 
     move(board, tileCoordinates) {
@@ -61,6 +60,77 @@ class Solver {
                     }
                     this.seen[newBoard.state] = true
                     this.queue.push(newBoard)
+                }
+            }
+        }
+        console.log('UNSOLVEABLE')
+    }
+}
+
+class AdvancedSolver extends Solver {
+    constructor(state) {
+        super(state)
+        this.queue = new PriorityQueue()
+    }
+
+    enqueueFirstBoard() {
+        // create the first board instance of starting state
+        let firstBoard = new Board(this.state, false)
+        if (firstBoard.isSolved()) {
+            this.solution = []
+            return
+        }
+        // find the zero location (only once)
+        firstBoard.zeroLoc = firstBoard.findZeroLoc()
+        firstBoard.manDist = firstBoard.manhattanDistance()
+        firstBoard.moves = []
+        this.queue.push(firstBoard, firstBoard.manDist)
+        this.seen[firstBoard.state] = true
+    }
+
+    getNextManDist(board, move) {
+        // get the number that is in this position
+        let currentVal = board.state[move[0]][move[1]]
+        // calculate the desired x and y positions of the number
+        let desY = Math.floor((currentVal - 1) / this.width)
+        let desX = (currentVal - 1) % this.width
+        // add the differences of actual and desired vals to dist
+        const currentDist = Math.abs(move[1] - desX) + Math.abs(move[0] - desY)
+        const newDist = Math.abs(board.zeroLoc[1] - desX) + Math.abs(board.zeroLoc[1] - desY)
+        return board.manDist - (currentDist - newDist)
+    }
+
+    getNextStates(board) {
+        // get the next board states but update their manhattan distances as well
+        let nextMoves = board.getNextMoves()
+        let newBoards = []
+        for (let i = 0; i < nextMoves.length; i++) {
+            let newBoard = this.move(board, nextMoves[i])
+            // newBoard.manDist = this.getNextManDist(board, nextMoves[i])
+            newBoards.push(newBoard)
+        }
+        return newBoards
+    }
+
+    solve() {
+        this.enqueueFirstBoard()
+        if (this.solution != null) {
+            return this.solution
+        }
+        while (!this.queue.empty()) {
+            const currBoard = this.queue.pop().data
+            // console.log('current board mandist', currBoard.manDist)
+            const newBoards = this.getNextStates(currBoard)
+            for (let i = 0; i < newBoards.length; i++) {
+                let newBoard = newBoards[i]
+                if (this.seen[newBoard.state] === undefined) {
+                    if (newBoard.isSolved()) {
+                        this.solution = newBoard.moves
+                        return newBoard.moves
+                    }
+                    this.seen[newBoard.state] = true
+                    // console.log("man dist", newBoard.manDist)
+                    this.queue.push(newBoard, newBoard.manhattanDistance())
                 }
             }
         }
